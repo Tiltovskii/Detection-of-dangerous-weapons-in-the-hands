3f9a4d17bf48d28950fe39958690a438dc647ccf
Slava <benderabenderabendera@gmail.com>
2023-01-04 12:39:37 +0300

    Renew wb

--- Web.py
+++ Web.py
@@ -57,10 +57,10 @@ def print_image(img, boxes, conf, labels):
                       font=ImageFont.truetype('arial.ttf', size=fort_size),
                       fill='white')
         except:
-
+            font = ImageFont.load_default()
             draw.text((boxes[indx][0] + 5, boxes[indx][1] - 2 * fort_size - 5),
                       f"Label: {translate[label]}\nConf: {conf[indx]:.2f}",
-                      font=ImageFont.truetype('arial.ttf', size=fort_size),
+                      font=font,
                       fill='white')
 
     st.image(img)
